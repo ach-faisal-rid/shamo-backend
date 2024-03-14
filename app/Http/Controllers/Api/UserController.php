d2be89b13e3f8dcc34a490c27dcc1cd7fa35d4ca
ach-faisal-rid <faizal.a.ridhani@gmail.com>
2024-03-14 21:24:05 +0700

    auth user registrasi bagian 2 menambahkan regex

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\Controller;
 use App\Helpers\ResponseFormatter;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\ValidationException;
 
 class UserController extends Controller
 {
@@ -16,15 +18,17 @@ class UserController extends Controller
             if(!$request->filled(['name', 'email', 'password'])) {
                 return response()->json(['message' => 'semua field harus diisi'], 422);
             }
-            
+
             // validasi request
             $request->validate([
-                'name' => ['required', 'string', 'max:255'],
-                'username' => ['required', 'string', 'max:255', 'unique:users'],
+                'name' => ['required', 'string', 'max:255', 'htmlspecialchars'],
+                'username' => ['required', 'string', 'max:255', 'unique:users', 'htmlspecialchars'],
                 'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
                 'phone' => ['nullable', 'string', 'max:255'],
-                'password' => 'required|min:6',
+                'password' => 'required|string|min:8|regex:/^.*(?=.{3,})(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*?&])[a-zA-Z0-9@$!%*?&].*$/',
             ]);
+
+            // Implementasi logika verifikasi email di sini (tidak ditampilkan)
 
             // Buat pengguna baru
             $user = User::create([
@@ -43,11 +47,18 @@ class UserController extends Controller
                 'token_type' => 'Bearer',
                 'user' => $user
             ],'User Registered');
-        } catch (Exception $error) {
+        } catch (ValidationException $e) {
             return ResponseFormatter::error([
-                'message' => 'Something went wrong',
-                'error' => $error,
-            ],'Authentication Failed', 500);
-        }
+              'message' => 'Validasi gagal',
+              'errors' => $e->errors(),
+            ], 'Registrasi Gagal', 422);
+          } catch (Exception $e) {
+            // Catat error untuk debugging
+            Log::error($e);
+
+            return ResponseFormatter::error([
+              'message' => 'Terjadi kesalahan',
+            ], 'Registrasi Gagal', 500);
+          }
     }
 }
