5aa36577e9ab840f013af02e78801f4bbf14228e
ach-faisal-rid <faizal.a.ridhani@gmail.com>
2024-03-15 15:19:07 +0700

    transaction bagian 1

---
+++ app/Http/Controllers/Api/TransactionController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Helpers\ResponseFormatter;
+use App\Models\transaction;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class TransactionController extends Controller
+{
+    public function all(Request $request) {
+        $id = $request->input('id');
+        $limit = $request->input('limit', 6);
+        $status = $request->input('status');
+
+        if($id) {
+            $product = transaction::with(['items.product'])
+                ->find($id);
+
+            if($product) {
+                return ResponseFormatter::success(
+                    $product,
+                    'Data Transaksi berhasil diambil'
+                );
+            }else {
+                return ResponseFormatter::error(
+                    null,
+                    'Data Transaksi tidak ada',
+                    404
+                );
+            }
+        }
+        $transaction = transaction::with(['items.product'])
+        ->where('usersID', Auth::user()->id);
+
+        if ($status) {
+            $transaction->where('status', $status);
+        }
+
+        return ResponseFormatter::success(
+            $transaction->paginate($limit),
+            'data list transaksi  berhasil diambil'
+        );
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\ProductCategoryController;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\TransactionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,6 +21,7 @@ use App\Http\Controllers\Api\UserController;
 
 Route::get('products', [ProductController::class, 'all']);
 Route::get('categories', [ProductCategoryController::class, 'all']);
+Route::get('transactions', [TransactionController::class, 'all']);
 
 Route::post('registrasi', [UserController::class, 'registrasi']);
 Route::post('login', [UserController::class, 'login']);
@@ -27,4 +29,5 @@ Route::post('login', [UserController::class, 'login']);
 Route::prefix('user')->middleware('auth:sanctum')->group(function () {
     Route::get('profile', [UserController::class, 'fetch']);
     Route::put('update-profile', [UserController::class, 'updateProfile']);
+    Route::post('logout', [UserController::class, 'logout']);
 });
